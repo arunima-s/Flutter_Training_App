81d3fee74f1f79f4c1dbd92d83f15f7e5c41671b
arunima-s <73657203+arunima-s@users.noreply.github.com>
2022-05-03 12:52:17 +0530

    rowncolumn

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:training_app/screens/homeScreen.dart';
+import 'package:training_app/screens/columnlayout.dart';
+import 'package:training_app/screens/rowlayout.dart';
+import 'package:training_app/screens/rowncolumn.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,21 +11,10 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
-  @override
+  @override  
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  // const MyHomePage({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text("Hello Myr"),
+      home: RownColumn(),
     );
   }
 }
